012efdf1919ef605273e24ab1f18790c068dd109
vinicius <vinicius123lopes@gmail.com>
2026-04-06 17:24:14 -0300

    ex75

--- ex75.py
+++ ex75.py
@@ -1,3 +1,14 @@
 tupla = ((int(input('Digite um numero: '))), (int(input('Digite um numero: '))), (int(input('Digite um numero: '))), (int(input('Digite um numero: '))))
 print(tupla)
 
+print(f'O numero nove aparece {tupla.count(9)} vezes')
+if 3 in tupla:
+    print(f'O numero tres aparece pela primeira vez na posicao {tupla.index(3)}')
+else:
+    print('O numero 3 nao aparece na tupla')
+for i in tupla:
+    conta = i % 2
+    if conta == 0:
+        print(f'O numero {i} eh par')
+    else:
+        print(f'O numero {i} eh impar')
